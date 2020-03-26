744386f2cf3f8d1f27962feeb854d6a64f7a1de3
Rohit Agarwal <60344453+rohit9798@users.noreply.github.com>
2020-03-26 20:24:09 +0530

    Create LRUCache.cpp

---
+++ Heaps And Maps/LRUCache.cpp
@@ -0,0 +1,67 @@
+Design and implement a data structure for LRU (Least Recently Used) cache. It should support the following operations: get and set.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+set(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least recently used item before inserting the new item.
+The LRU Cache will be initialized with an integer corresponding to its capacity. Capacity indicates the maximum number of unique keys it can hold at a time.
+
+Definition of “least recently used” : An access to an item is defined as a get or a set operation of the item. “Least recently used” item is the one with the oldest access time.
+
+ NOTE: If you are using any global variables, make sure to clear them in the constructor. 
+Example :
+
+Input : 
+         capacity = 2
+         set(1, 10)
+         set(5, 12)
+         get(5)        returns 12
+         get(1)        returns 10
+         get(10)       returns -1
+         set(6, 14)    this pushes out key = 5 as LRU is full. 
+         get(5)        returns -1 
+
+
+
+#include<list>
+unordered_map<int, int> m;
+list<int> l;
+int cap;
+LRUCache::LRUCache(int capacity) {
+    
+    m.clear();
+    l.clear();
+    cap = capacity;
+}
+
+int LRUCache::get(int key) {
+
+    if(m.count(key) > 0)
+    {
+        l.remove(key);
+        l.push_front(key);
+        return m[key];
+    }
+    else
+        return -1;
+}
+
+void LRUCache::set(int key, int value) {
+    
+    if(m.size() == cap && m.count(key) == 0)
+    {
+        int replace = l.back();
+        m.erase(replace);
+        l.remove(replace);
+        l.push_front(key);
+        m[key] = value;
+    }
+    else
+    {
+        if(m.count(key) > 0)
+        {
+            l.remove(key);
+            l.push_front(key);
+        }
+        m[key] = value;
+        l.push_front(key);
+    }
+}
